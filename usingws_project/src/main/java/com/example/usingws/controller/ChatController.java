41b145d987a22ba694cb52eb81b5806eda466894
kkhs00224 <kkhs00224@gmail.com>
2023-05-09 00:31:27 +0900

    채팅방 수정

--- usingws_project/src/main/java/com/example/usingws/controller/ChatController.java
+++ usingws_project/src/main/java/com/example/usingws/controller/ChatController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
+import java.util.UUID;
+
 @Controller
 @Log4j2
 public class ChatController {
@@ -17,12 +19,14 @@ public class ChatController {
                           Model model){
         log.info("@ChatController, chat Get()");
         String user = "";
+        UUID uniqueKey = UUID.randomUUID();
         if(loginUser != null){
             user = loginUser.getUserId();
         }else{
-            user = "Unknown";
+            user = "Unknown++"+uniqueKey;
         }
         model.addAttribute("user", user);
+        model.addAttribute("uniqueKey", uniqueKey);
         return "chat/chat";
     }
 }
